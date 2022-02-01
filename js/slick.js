e8ce68be0e8e2009375ac4e9dd88e28068e645d9
German Dleonardi <dleonardi.german@gmail.com>
2022-01-31 22:22:44 -0300

    subiendo archivos con bootstrap

---
+++ js/slick.js
@@ -0,0 +1,45 @@
+$(function (){
+
+  $('.product-container').slick({
+    dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<span class="priv_arrow"><i class="fas fa-chevron-circle-left"></i></span>',
+    nextArrow: '<span class="next_arrow"><i class="fas fa-chevron-circle-right"></i></span>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    
+     });
+
+
+    
+
+});
